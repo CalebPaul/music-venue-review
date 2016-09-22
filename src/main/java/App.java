6c17a4b5fb53cdd40afde51919774699092bd9a6
Chance Neff and Caleb Paul <caleb@>
2016-09-22 16:33:48 -0700

    add partial front end functionality

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,3 +3,79 @@ import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      model.put("cities", City.all()); //not currently being used.
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities/:id/venues/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city-venue-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities/:id/venues", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("venues", Venue.all());
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities/new", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      model.put("template", "templates/city-form.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      model.put("cities", City.all());
+      model.put("template", "templates/cities.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/cities/:id", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      City city = City.find(Integer.parseInt(request.params(":id")));
+      model.put("city", city);
+      model.put("template", "templates/city.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/cities", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      String name = request.queryParams("name");
+      City newCity = new City(name);
+      newCity.save();
+      model.put("cities", City.all());
+      model.put("template", "templates/cities.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/venues", (request, response) -> {
+      Map<String, Object> model = new HashMap<>();
+      City city = City.find(Integer.parseInt(request.queryParams("cityId")));
+      String name = request.queryParams("name");
+      String description = request.queryParams("description");
+      Integer capacity = Integer.parseInt(request.queryParams("capacity"));
+      Venue newVenue = new Venue(name, description, capacity, city.getId());
+      newVenue.save();
+      model.put("city", city);
+      model.put("template", "templates/venues.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+  }
+}
